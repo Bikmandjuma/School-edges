628c8102d7fa919c9013435d57d2384af9315604
bugarama123@ <bikmangeek@gmail.com>
2024-08-01 14:32:20 +0200

    tooltip added in admin panel

--- laravel_api/resources/views/admin/view_users.blade.php
+++ laravel_api/resources/views/admin/view_users.blade.php
@@ -17,8 +17,22 @@
 
           <div class="card">
             <div class="card-body" style="overflow: auto;">
-              <h5 class="card-title">System users</h5>
-              <!-- Table with stripped rows -->
+            	<div class="row">
+        			<div class="col-lg-8">
+        				<h5 class="card-title">System users info</h5>
+        			</div>
+        			<div class="col-lg-4" style="display: flex; justify-content: center;">
+  						<div class="filter mt-2" style="display: flex; flex-direction: column; align-items: center;">
+		                  <a class="icon btn btn-primary" href="#" data-bs-toggle="dropdown"><i class="fa fa-user"></i>&nbsp;<span style="font-family: sans-serif;font-style: italic;y">Add new user</span></a>
+		                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+		                    <li><a class="dropdown-item" href="#" data-bs-toggle="tooltip" data-bs-placement="left" title="Add new system user by his/her full information">By full info</a></li>
+		                    <li><a class="dropdown-item" href="#" data-bs-toggle="tooltip" data-bs-placement="left" title="Sent a user an email link to register him/her self !">By Email</a></li>
+		                  </ul>
+		                </div>
+        			</div>
+        		</div>
+              
+                
               <table class="table datatable">
                 <thead>
                   <tr>
