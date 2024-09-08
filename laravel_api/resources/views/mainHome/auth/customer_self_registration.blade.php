cfaf4c7985b693908d657092852bd2ddefd20d59
BikmanDjuma <bikmangeek@gmail.com>
2024-09-08 20:41:46 +0200

    work on customer register

--- laravel_api/resources/views/mainHome/auth/customer_self_registration.blade.php
+++ laravel_api/resources/views/mainHome/auth/customer_self_registration.blade.php
@@ -1,122 +1,69 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Customer registration</title>
-	<style type="text/css">
-		/*{
-		    padding: 0;
-		    margin: 0;
-		    box-sizing: border-box;
-		}*/
-		html, body{
-		    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-		    background-color:#eee; 
-		    height:100%;
-		}
-		.container{ 
-		   height:100%;
-		   display:flex;
-		   align-items:center;
-		   justify-content:center;
-		}
-		.content{
-		    background-color:white;
-		    width:500px;
-		    height:500px;
-		    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-		}
-		img{
-		    width: 100%;
-		    height: 25%;
-		}
-		.form-title{
-		    padding:10px 40px 0px;
-		}
-		form{
-		    padding:0px 40px;
-		}
-		input[type=text], [type=email]{
-		    border: none;
-		    border-bottom: 1px solid black;
-		    outline:none;
-		    width:100%;
-		    margin: 8px 0;
-		    padding:10px 0;
-		}
-		input[type=number]{
-		    border: none;
-		    border-bottom: 1px solid black;
-		    outline:none;
-		    margin: 8px 0;
-		    padding:5px 0;
-		}
-		input :hover {
-		    background-color: red;
-		}
-		select{
-		    border: none;
-		    border-bottom: 1px solid black;
-		    outline:none;
-		    margin: 8px 0;
-		    padding:5px 0;
-		    width:50%;
-		}
-		.beside{
-		    display:flex;  
-		    justify-content: space-between;
-		}
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up Form by Colorlib</title>
 
-		.besidex{
-		    display:flex;  
-		    justify-content: space-between;
-		}
-		button{
-		    color:#ffffff;
-		    background-color: #4caf50;
-		    height:40px;
-		    width:25%;
-		    margin-top:15px;
-		    cursor: pointer;
-		    border:none;
-		    border-radius:2%;
-		    outline:none;
-		    text-align:center;
-		    font-size:16px;
-		    text-decoration:none;
-		    -webkit-transition-duration:0.4s;
-		    transition-duration:0.4s;
-		}
-		button:hover{
-		    background-color:#333333;
-		}
-	</style>
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="{{URL::to('/')}}/CustomerSelfRegister/fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="{{URL::to('/')}}/CustomerSelfRegister/css/style.css">
 </head>
 <body>
 
-	<div class="container">
-	    <div class="content">
-	       <img src="https://res.cloudinary.com/debbsefe/image/upload/f_auto,c_fill,dpr_auto,e_grayscale/image_fz7n7w.webp" alt="header-image" class="cld-responsive">
-	            <h1 class="form-title">Register Here</h1>
-	            <form>
-	                <input type="text" placeholder="NAME">
-		            
-		            <div class="beside">
-		                <input type="number" placeholder="PHONE NUMBER">
-		                <select>
-		                    <option>GENDER</option>
-		                    <option>MALE</option>
-		                    <option>FEMALE</option>
-		                    <option>NON-BINARY</option>
-		                </select>
-		            </div>
-      
-	                <button type="button">Submit</button>
-	                
-	            </form>
-	        </div>
-	 </div>
+    <div class="main">
 
-</body>
+        <!-- Sign up form -->
+        <section class="signup">
+            <div class="container" style="margin-top:-10%;box-shadow:0px 8px 16px 0px rgba(0, 0, 0, 0.2);">
+                <div class="signup-content">
+                    <div class="signup-form">
+                        <h2 class="form-title">Register here</h2>
+                        <form method="POST" class="register-form" id="register-form">
+                            <div class="form-group">
+                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="school_name" id="name" placeholder="Enter school name" value="{{ $school_name }}" />
+                            </div>
+                            <div class="form-group">
+                                <label for="name"><i class="zmdi zmdi-email material-icons-name"></i></label>
+                                <input type="email" name="email" id="name" placeholder="Enter email" value="{{ $email }}" disabled />
+                            </div>
+                            <div class="form-group">
+                                <label for="email"><i class="zmdi zmdi-phone"></i></label>
+                                <input type="number" name="phone" id="phone" placeholder="Enter phone" value="{{ $phone }}" disabled />
+                            </div>
+                            <div class="form-group">
+                                <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="pass" id="pass" placeholder="Password"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
+                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password"/>
+                            </div>
+                            <!-- <div class="form-group">
+                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                                <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
+                            </div> -->
+                            <div class="form-group form-button">
+                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="signup-image">
+                        <figure><img src="{{URL::to('/')}}/CustomerSelfRegister/images/signup-image.jpg" alt="sing up image"></figure>
+                        <p class="signup-image-link" style="text-decoration: none;color:gray;">You can modify your info in your account after registration !</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+
+    <!-- JS -->
+    <script src="{{URL::to('/')}}/CustomerSelfRegister/vendor/jquery/jquery.min.js"></script>
+    <script src="{{URL::to('/')}}/CustomerSelfRegister/js/main.js"></script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 </html>
