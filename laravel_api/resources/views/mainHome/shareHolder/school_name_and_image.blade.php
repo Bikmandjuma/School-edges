f2eeba5d1566e1a651b58a736b072decc5c72e61
BikmanDjuma <bikmangeek@gmail.com>
2024-09-21 14:12:15 +0200

    work on shareHolders panel

---
+++ laravel_api/resources/views/mainHome/shareHolder/school_name_and_image.blade.php
@@ -0,0 +1,16 @@
+<div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
+    <img src="{{ URL::to('/') }}/mainHomePage/img/school/{{ $school_data->image }}" alt="Profile" class="rounded-circle" style="border:4px solid #eee;">
+    @if(strlen($school_data->school_name."Bikman") > 18)
+        <h2 title="{{ $school_data->school_name }}">{{ substr($school_data->school_name,0,18).'..' }}</h2>
+    @else
+        <h2 title="{{ $school_data->school_name }}">{{ $school_data->school_name }}</h2>
+    @endif
+    <h3 class="mt-2">School , <span style="font-family: sans-serif;color:blueviolet;font-style: italic;">{{ $school_data->school_code }}</span></h3>
+    <div class="social-links mt-2">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-whatsapp"></i></a>
+    </div>
+</div>
--- laravel_api/resources/views/mainHome/shareHolder/view_single_school_info.blade.php
+++ laravel_api/resources/views/mainHome/shareHolder/view_single_school_info.blade.php
@@ -15,22 +15,7 @@
         <div class="row">
             <div class="col-xl-4">
                 <div class="card">
-                    <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
-                        <img src="{{ URL::to('/') }}/mainHomePage/img/school/{{ $school_data->image }}" alt="Profile" class="rounded-circle" style="border:4px solid #eee;">
-                        @if(strlen($school_data->school_name."Bikman") > 18)
-                            <h2 title="{{ $school_data->school_name }}">{{ substr($school_data->school_name,0,18).'..' }}</h2>
-                        @else
-                            <h2 title="{{ $school_data->school_name }}">{{ $school_data->school_name }}</h2>
-                        @endif
-                        <h3 class="mt-2">School , <span style="font-family: sans-serif;color:blueviolet;font-style: italic;">{{ $school_data->school_code }}</span></h3>
-                        <!--div class="social-links mt-2">
-                            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-                            <a href="#" class="linkedin"><i class="bi bi-whatsapp"></i></a>
-                        </div-->
-                    </div>
+                    @include('mainHome.shareHolder.school_name_and_image')
                 </div>
             </div>
 
@@ -38,20 +23,7 @@
                 <div class="card">
                     <div class="card-body pt-3">
                         <!-- Bordered Tabs -->
-                        <ul class="nav nav-tabs nav-tabs-bordered">
-                            <li class="nav-item">
-                                <button class="nav-link {{ Request::segment(2) == 'view_single_school_info' ? 'active' : '' }}" data-bs-toggle="tab" data-bs-target="#profile-overview" onclick="window.location.href='{{ route('main.show.profile',Crypt::encrypt($school_data->id)) }}'">Overview</button>
-                            </li>
-                            <li class="nav-item">
-                                <button class="nav-link {{ Request::segment(2) == 'information' ? 'active' : '' }}" onclick="window.location.href='{{ route('main.Customer_edit_info',Crypt::encrypt($school_data->id)) }}'">Edit info</button>
-                            </li>
-                            <li class="nav-item">
-                                <button class="nav-link {{ Request::segment(2) == 'username' ? 'active' : '' }}" onclick="window.location.href='{{ route('main.Customer_employee_student',Crypt::encrypt($school_data->id)) }}'">Employees & students</button>
-                            </li>
-                            <li class="nav-item">
-                                <button class="nav-link {{ Request::segment(2) == 'password' ? 'active' : '' }} " onclick="window.location.href='{{ route('main.Customer_payment_status',Crypt::encrypt($school_data->id)) }}'">Payment & Contract</button>
-                            </li>
-                        </ul>
+                        @include('mainHome.shareHolder.main_tabs_links_info')
                         <div class="tab-content pt-2">
                             <div class="tab-pane fade show active profile-overview" id="profile-overview">
                                 <h5 class="card-title">School Details</h5>
